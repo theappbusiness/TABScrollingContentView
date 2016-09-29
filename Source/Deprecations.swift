0ab3d5bd20b49e77cf04097aeb586be1588770dc
Daniela Bulgaru <danielablg@gmail.com>
2016-09-29 14:38:06 +0100

    Added Deprecations file

---
+++ Source/Deprecations.swift
@@ -0,0 +1,9 @@
+//
+//  Deprecations.swift
+//  TABScrollingContentView
+//
+//  Created by Daniela Bulgaru on 26/09/2016.
+//  Copyright © 2016 TheAppBusiness. All rights reserved.
+//
+
+import Foundation
